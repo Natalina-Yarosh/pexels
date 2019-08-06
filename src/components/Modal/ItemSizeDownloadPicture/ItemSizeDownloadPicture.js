d52bb9f293d9ea2d5c79eb12bc685f8fb8b8bfbc
Natalia <natali.yarosh.96@mail.ru>
2019-08-06 14:27:17 +0300

    pexels/add: download picture

--- src/components/Modal/ItemSizeDownloadPicture/ItemSizeDownloadPicture.js
+++ src/components/Modal/ItemSizeDownloadPicture/ItemSizeDownloadPicture.js
@@ -1,41 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './ItemSizeDownloadPicture.module.css';
+import download from 'downloadjs';
 
-const ItemSizeDownloadPicture = () => {
+const ItemSizeDownloadPicture = ({resultIdRender}) => {
+   
+    console.log(resultIdRender)
+
+    const [changeSize, setChangeSize] = useState()
+
+    const chekedSize = (e) => {
+        let value = e.target.value        
+        setChangeSize(changeSize => value)        
+    }
+
+    const downloadPicture = () => {              
+        download(resultIdRender[0].src[changeSize]);
+    }
+    
     return (
         <div className = {style.listDownloadPicture}>
             <form >
                     <h3 className = {style.listTitle}>Choose a size:</h3>
                     <ul className = {style.listAll}>
-                        <li className = {style.listItem}>                            
-                                <input type="radio" value="5040x3360"/>
-                                <span className = {style.itemName}><strong>Original</strong> (5040 x 3360)</span>              
+
+                        <li className = {style.listItem}>    
+                            <label>                         
+                                <input name = 'picture' type="radio"  value='original'  
+                                 onChange = {(e) => {chekedSize(e)}}/>
+                                <span className = {style.itemName}><strong>Original</strong> (5040 x 3360)</span>
+                            </label>              
+                        </li>
+
+                        <li className = {style.listItem}>   
+                            <label>                      
+                                <input name = 'picture' type="radio" value='large'
+                                 onChange = {(e) => {chekedSize(e)}} />
+                                <span className = {style.itemName}><strong>Large</strong> (1920 x 1280)</span>
+                            </label> 
+                        </li>
+
+                        <li className = {style.listItem}>  
+                            <label>                         
+                                <input name = 'picture' type="radio" value='medium'
+                                 onChange = {(e) => {chekedSize(e)}}/>
+                                <span className = {style.itemName}><strong>Medium</strong> (1280 x 853)</span>
+                            </label>
                         </li>
 
                         <li className = {style.listItem}>
-                          
-                            <input type="radio" value="1920x1280"/>
-                            <span className = {style.itemName}><strong>Large</strong> (1920 x 1280)</span>
-                            
-                        </li>
-
-                        <li className = {style.listItem}>
-                            
-                            <input type="radio" value="1280x853"/>
-                            <span className = {style.itemName}><strong>Medium</strong> (1280 x 853)</span>
-                            
-                        </li>
-
-                        <li className = {style.listItem}>
-                        <label>
-                        <input type="radio" value="640x426"/>
-                        <span className = {style.itemName}><strong>Small</strong> (640 x 426)</span>
-                        </label>
+                            <label>
+                                <input name = 'picture'  type="radio" value='small'
+                                 onChange = {(e) => {chekedSize(e)}} />
+                                <span className = {style.itemName}><strong>Small</strong> (640 x 426)</span>
+                            </label>
                         </li>
                         
                         </ul>
                         
-                        <button className={style.iconDownload} > Free Download</button>
+                        <button className={style.iconDownload} onClick = {downloadPicture} type="button"> Free Download</button>
                     
             </form>
         </div>
